9e3f656b52e857df86296b5bc258e55d0bca07a4
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-04-03 23:08:33 +0300

    f10

--- test.mk
+++ test.mk
@@ -4,7 +4,7 @@ PROJECT_PATH_ENV=/midi-generator
 .PHONY: test_jupyter_demo
 test_jupyter_demo:
 	make jupyter \
-         JUPYTER_CMD=" \
+         JUPYTER_CMD="bash -c ' \
              pip install --progress-bar=off pandoc && \
              jupyter nbconvert \
              --execute \
@@ -13,4 +13,4 @@ test_jupyter_demo:
              --to=asciidoc \
              --output=out \
              $(PROJECT_PATH_ENV)/$(NOTEBOOKS_DIR)/inference.ipynb \
-        "
+        '"
