2d8f85c0b6c57ac212a6d729dff62512bec794ae
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-04-03 22:31:10 +0300

    fix5

--- test.mk
+++ test.mk
@@ -2,7 +2,6 @@
 # TEST_ID=$(shell date +%Y%m%d=%H%M%S-%N)
 # PROJECT=ml-recipe-midi-generator
 # IMAGE_URI=image:$(PROJECT):e2e-$(TEST_ID)
-# NOTEBOOKS_DIR=notebooks
 #
 # setup:
 # 	make setup \
@@ -10,14 +9,18 @@
 # 		CUSTOM_ENV_NAME=$(IMAGE_URI) \
 # 		PROJECT_PATH_STORAGE=storage:$(PROJECT)/tests/e2e/$(TEST_ID)
 
+NOTEBOOKS_DIR=notebooks
+PROJECT_PATH_ENV=/midi-generator
+
 .PHONY: test_jupyter_demo
 test_jupyter_demo:
 	make jupyter \
-		JUPYTER_CMD="jupyter nbconvert \
-			--execute \
-			--no-prompt \
-			--no-input \
-			--to=asciidoc \
-			--output=out \
-			$(NOTEBOOKS_DIR)/inference.ipynb \
+         JUPYTER_CMD=" \
+             jupyter nbconvert \
+             --execute \
+             --no-prompt \
+             --no-input \
+             --to=asciidoc \
+             --output=out \
+             $(PROJECT_PATH_ENV)/$(NOTEBOOKS_DIR)/inference.ipynb \
         "
