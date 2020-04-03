5114eee0b02f50e199919f23d3a244ed5fa3515c
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-04-03 22:50:21 +0300

    clenaup

--- test.mk
+++ test.mk
@@ -1,14 +1,3 @@
-#
-# TEST_ID=$(shell date +%Y%m%d=%H%M%S-%N)
-# PROJECT=ml-recipe-midi-generator
-# IMAGE_URI=image:$(PROJECT):e2e-$(TEST_ID)
-#
-# setup:
-# 	make setup \
-# 		__BAKE_SETUP=yes \
-# 		CUSTOM_ENV_NAME=$(IMAGE_URI) \
-# 		PROJECT_PATH_STORAGE=storage:$(PROJECT)/tests/e2e/$(TEST_ID)
-
 NOTEBOOKS_DIR=notebooks
 PROJECT_PATH_ENV=/midi-generator
 
@@ -16,7 +5,7 @@ PROJECT_PATH_ENV=/midi-generator
 test_jupyter_demo:
 	make jupyter \
          JUPYTER_CMD=" \
-             pip install pandoc && \
+             pip install --progress-bar=off pandoc && \
              jupyter nbconvert \
              --execute \
              --no-prompt \
